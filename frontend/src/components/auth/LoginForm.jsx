8ba1599dc269a5d2c710ffac2e7fdc3c6832e709
beca <rebecamarcial648@gmail.com>
2025-03-30 13:14:07 -0600

    background en login

--- frontend/src/components/auth/LoginForm.jsx
+++ frontend/src/components/auth/LoginForm.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Dropdown from '../Dropdown';
-import logoCFE from '/src/assets/logoCFE.png'
+import logoCFE from '/src/assets/logoCFE.png';
+import fondoLogin from '/src/assets/fondoLogin.webp';
 
 
 const LoginForm = ({ 
@@ -15,8 +16,11 @@ const LoginForm = ({
     handleSubmit,
     error 
 }) => (
+    <div className='relative flex items-center justify-center min-h-screen bg-cover bg-center' style={{ backgroundImage: `url(${fondoLogin})` }}>
+        <div className="absolute inset-0 bg-black opacity-50"></div>
 
-    <div className='flex flex-col items-center justify-center min-h-screen'>
+    
+        <div className='relative flex flex-col items-center justify-center min-h-screen w-full backdrop-blur-md'>
         <img src={logoCFE} alt="Logo CFE" className="w-36 h-auto mt-6 mb-4 md:w-48 md:mt-8 md:mb-5 lg:w-56 lg:mt-10 lg:mb-6" />
 
         <div className="relative flex flex-col m-6 space-y-8 bg-white shadow-2xl rounded-2xl md:flex-row md:space-y-0 md:w-3/4 lg:w-1/2 xl:w-1/3">
@@ -25,11 +29,11 @@ const LoginForm = ({
                 <span className="mb-2 text-xl font-semibold text-gray-600 md:text-2xl lg:text-3xl">Gestion de Incidencias</span>
                 <span className="font-normal text-xs text-gray-500 mb-10 md:text-sm lg:text-base">Selecciona la información solicitada</span>
 
-                <div>
+         
                     
                     <form onSubmit={handleSubmit} className="flex flex-col gap-3 w-full">
                         <Dropdown 
-                            label="Selecciona un rol" 
+                            label="Rol" 
                             options={roles} 
                             onChange={handleRolChange} 
                         />
@@ -63,6 +67,8 @@ const LoginForm = ({
             
         </div>
     </div>
+
+
 
 
 );
