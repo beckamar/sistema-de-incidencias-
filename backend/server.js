0e6c6bed805fe68b71cfa7c4c5fb7dfbb25a1681
beca <rebecamarcial648@gmail.com>
2025-05-06 11:51:23 -0600

    Deploy configuracion

--- backend/server.js
+++ backend/server.js
@@ -7,17 +7,27 @@ import v1Router from "./src/v1/v1Routes.js";
 import FirebaseRoute from "./src/v1/FirebaseRoute.js";
 import swaggerUI from 'swagger-ui-express';
 import swaggerDocumentation from './swagger.json' with {type: 'json'};
+import path from "path";
+import { fileURLToPath } from 'url';
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
 app.use(express.json());
 app.use(cors());
 
 app.use("/api", swaggerUI.serve, swaggerUI.setup(swaggerDocumentation));
-
 app.use("/v1", v1Router); 
 app.use("/firebase", FirebaseRoute);
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+  });
 
 app.use(errorHandling);
 
